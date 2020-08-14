c92d38616808f887597befe203275e3167f18a2d
Proger3228 <damir221003@gmail.com>
2020-08-14 18:10:22 +0400

    Fixed error loging in vk api

--- VkAPI/VK_API.js
+++ VkAPI/VK_API.js
@@ -1,102 +1,121 @@
 // @ts-nocheck
-const { createVkApi } = require( "./apiCreator" );
-const fetch = require( "node-fetch" );
-const config = require( "config" );
-const FormData = require( "form-data" );
-const path = require( 'path' );
-const fs = require( "fs" );
+const { createVkApi } = require("./apiCreator");
+const fetch = require("node-fetch");
+const config = require("config");
+const FormData = require("form-data");
+const path = require("path");
+const fs = require("fs");
 
 class VK_API {
     apiKey;
     api;
-    constructor( key, groupId, albumId ) {
+    constructor(key, groupId, albumId) {
         this.apiKey = key;
         this.groupId = groupId;
         this.albumId = albumId;
-        this.api = createVkApi( key );
+        this.api = createVkApi(key);
     }
 
-    async getPhotoUrl ( attachment, album_id ) {
+    async getPhotoUrl(attachment, album_id) {
         try {
-            if ( attachment ) {
+            if (attachment) {
                 let url;
-                if ( /^photo/.test( attachment ) ) {
-                    const [ owner_id, photo_ids ] = attachment.slice( 5 ).split( "_" );
-                    url = await this.api( "photos.get", {
+                if (/^photo/.test(attachment)) {
+                    const [owner_id, photo_ids] = attachment
+                        .slice(5)
+                        .split("_");
+                    url = await this.api("photos.get", {
                         owner_id,
                         photo_ids,
-                        album_id: album_id
-                    } ).then( photo => photo.items[ 0 ].sizes[ 5 ].url );
+                        album_id: album_id,
+                    }).then((photo) => photo.items[0].sizes[5].url);
                 }
 
                 return url;
             } else {
                 return "";
             }
-        } catch ( e ) {
-            console.error( e );
+        } catch (e) {
+            console.error(e);
         }
     }
-    async getUser ( userId ) {
+    async getUser(userId) {
         try {
-            return await this.api( "users.get", { user_ids: userId } )
-        } catch ( e ) {
-            console.error( e );
+            return await this.api("users.get", { user_ids: userId });
+        } catch (e) {
+            console.error(e);
         }
     }
 
-    async uploadPhotoToAlbum ( fileReadStream ) {
+    async uploadPhotoToAlbum(fileReadStream) {
         try {
-            const url = await this.getUploadServerUrl( config.get( "GROUP_ID" ), config.get( "ALBUM_ID" ) );
+            const url = await this.getUploadServerUrl(
+                config.get("GROUP_ID"),
+                config.get("ALBUM_ID")
+            );
             const formData = new FormData();
 
-            formData.append( "file", fileReadStream );
+            formData.append("file", fileReadStream);
 
-            const { aid: album_id, gid: group_id, hash, server, photos_list } = await this.uploadPhotoToServer( formData, url );
+            const {
+                aid: album_id,
+                gid: group_id,
+                hash,
+                server,
+                photos_list,
+            } = await this.uploadPhotoToServer(formData, url);
 
-            const res = this.savePhoto( { album_id, group_id, hash, server, photos_list } );
+            const res = this.savePhoto({
+                album_id,
+                group_id,
+                hash,
+                server,
+                photos_list,
+            });
 
             return res;
-        } catch ( e ) {
-            console.error( e );
+        } catch (e) {
+            console.error(e);
         }
     }
-    async uploadPhotoToServer ( formData, url ) {
+    async uploadPhotoToServer(formData, url) {
         try {
-            const response = await fetch( url, {
-                method: 'POST',
-                body: formData
-            } )
+            const response = await fetch(url, {
+                method: "POST",
+                body: formData,
+            });
             let vkr = await response.json();
 
-            if ( vkr ) {
+            if (vkr) {
                 return vkr;
             } else {
-                throw new Error( "Empty response" );
+                throw new Error("Empty response");
             }
-        } catch ( err ) {
-            console.error( e );
+        } catch (err) {
+            console.error(err);
         }
     }
 
-    async getUploadServerUrl ( group_id, album_id ) {
+    async getUploadServerUrl(group_id, album_id) {
         try {
-            return await this.api( "photos.getUploadServer", { group_id, album_id } )
-                .then( res => {
-                    return res.upload_url
-                } );
-        } catch ( e ) {
-            console.error( e )
+            return await this.api("photos.getUploadServer", {
+                group_id,
+                album_id,
+            }).then((res) => {
+                return res.upload_url;
+            });
+        } catch (e) {
+            console.error(e);
         }
     }
 
-    async savePhoto ( uploadedPhotosObject ) {
+    async savePhoto(uploadedPhotosObject) {
         try {
-            return await this.api( "photos.save", uploadedPhotosObject );
-        } catch ( e ) {
-            console.error( e );
+            return await this.api("photos.save", uploadedPhotosObject);
+        } catch (e) {
+            console.error(e);
         }
     }
-};
+}
 
 module.exports = VK_API;
