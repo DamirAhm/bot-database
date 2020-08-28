ec462882313dc1302ea4041b738b2b7f5994ac82
DamirAhm <damir221003@yandex.ru>
2020-08-28 12:14:01 +0400

    1.0.10

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { DataBase } = require( "./DataBase" );
 const ModelUtils = require( "./Models/utils" );
+const VK_API = require( "./VkAPI/VK_API" );
 
 module.exports = {
     DataBase,
+    VK_API,
     Roles: ModelUtils.Roles,
-    Lessons: ModelUtils.Lessons
+    Lessons: ModelUtils.Lessons,
 }
