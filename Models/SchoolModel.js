16f344df339d0693e9b2011c584197e7bef65020
DamirAhm <damir221003@yandex.ru>
2020-10-18 21:46:22 +0400

    Changed attachment string validation

--- Models/SchoolModel.js
+++ Models/SchoolModel.js
@@ -2,33 +2,11 @@ const { isURL } = require('./utils');
 
 const mongoose = require('mongoose');
 
-const attachment = new mongoose.Schema({
-	value: {
-		type: String,
-		validate: {
-			validator: (str) => /^photo.+(_.+)*/.test(str),
-			message: 'url must be a valid vk attachment',
-		},
-		required: true,
-	},
-	url: {
-		type: String,
-		validate: {
-			validator: isURL,
-			message: 'url must be a valid URL',
-		},
-		required: true,
-	},
-	album_id: {
-		type: Number,
-	},
-});
-
 const schoolSchema = new mongoose.Schema({
 	classes: {
 		type: [
 			{
-				type: mongoose.Schema.ObjectId,
+				type: mongoose.Types.ObjectId,
 				ref: 'Class',
 			},
 		],
@@ -42,27 +20,6 @@ const schoolSchema = new mongoose.Schema({
 		},
 		required: true,
 		unique: true,
-	},
-	announcements: {
-		type: [
-			{
-				text: String,
-				attachments: [attachment],
-				to: Date,
-				createdBy: {
-					type: Number,
-					validate: {
-						validator: Number.isInteger,
-						message: 'Created by must be integer means vk id of user created it',
-					},
-				},
-				_id: {
-					type: mongoose.Schema.Types.ObjectId,
-					default: new mongoose.Types.ObjectId(),
-				},
-			},
-		],
-		default: [],
 	},
 });
 
