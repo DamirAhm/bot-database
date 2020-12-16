e57898266a85dbf1c001b829d9d8c9307c019b31
DamirAhm <damir221003@yandex.ru>
2020-12-16 20:01:08 +0400

    3.1.9-0

--- VkAPI/apiCreator.ts
+++ VkAPI/apiCreator.ts
@@ -1,30 +1,33 @@
-import qs from "qs";
-import https from "https";
+import qs from 'qs';
+import https from 'https';
 
 export const createVkApi = (token: string) => {
 	return (method: string, pars: { [par: string]: string | number }) => {
-		pars.v = pars.v || "5.103";
+		pars.v = pars.v || '5.103';
 		return new Promise((resolve, reject) => {
 			const params = qs.stringify(pars);
-			https.get({
-				host: "api.vk.com",
-				path: `/method/${method}?${params}&access_token=${token}`
-			}, (res) => {
-				let resData = "";
-				res.on("data", data => resData += data.toString());
-				res.on("end", () => {
-					try {
-						const result = JSON.parse(resData);
-						if (result.error) {
-							reject(result.error)
-						} else {
-							resolve(result.response)
+			https.get(
+				{
+					host: 'api.vk.com',
+					path: `/method/${method}?${params}&access_token=${token}`,
+				},
+				(res) => {
+					let resData = '';
+					res.on('data', (data) => (resData += data.toString()));
+					res.on('end', () => {
+						try {
+							const result = JSON.parse(resData);
+							if (result.error) {
+								reject(result.error);
+							} else {
+								resolve(result.response);
+							}
+						} catch (e) {
+							reject(e.message);
 						}
-					} catch (e) {
-						reject(e.message)
-					}
-				})
-			})
-		})
-	}
+					});
+				},
+			);
+		});
+	};
 };
